ed77adb76823da2d4a80ff51b5552c45fe30bc28
Roberto <robpalacios1@hotmail.com>
2019-09-11 18:38:11 -0500

    Modify the Component to add tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React, { Component } from 'react';
+import React  from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      newTask : '',
+      tasks : [
+        'Sacar la ropa',
+        'Hacer la cama',
+        'Leer un rato'
+      ]
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleText = this.handleText.bind(this)
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.tasks.map(e => {
+              return <li key = {e}>{e}</li>
+            })}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit = {this.handleSubmit}>
+             <input
+             type="text"
+             id="new-task"
+             onChange = {this.handleText}
+             value = {this.state.newTask}
+             placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
-    )
+    );
+  }
+
+  handleText(event){
+    this.setState({newTask: event.target.value})
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    const newTask = this.state.newTask
+    const oldTask = this.state.tasks
+    if (this.state.task !== ''){
+      this.setState({
+        tasks: [...oldTask, newTask],
+        newTask: ''
+      })
+    }
   }
 }
 
